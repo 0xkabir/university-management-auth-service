deb1cad8825102e492d54ccce18b80d00db29a9e
0xkabir <kabir.sadiken1@gmail.com>
2023-06-23 01:10:52 +0600

    [Done] setup eslint, prettier, husky, lint-staged

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
-import config from "./config";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config'
+import app from './app'
 
 async function bootstrap() {
-    try{
-        await mongoose.connect(config.database_url as string)
-        console.log("Database Connection Successful");
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database Connection Successful')
 
-        app.listen(config.port, ()=>{
-            console.log(`Application Listening at port ${config.port}`);
-        })
-    }
-    catch(error){
-        console.log(`error connecting to database\n${error}`);
-    }
+    app.listen(config.port, () => {
+      console.log(`Application Listening at port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(`error connecting to database\n${error}`)
+  }
 }
 
-bootstrap();
+bootstrap()
