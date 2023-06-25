4896f71362a026ea882957584f71956289cdde73
0xkabir <kabir.sadiken1@gmail.com>
2023-06-25 12:23:56 +0600

    [Done] improve error handling setup for uncaught server rejection, uncaught exception and SIGTERM

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,8 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-expressions */
 import { ErrorRequestHandler } from 'express'
 import config from '../config'
 import { IGenericErrorMessages } from '../interfaces/error'
 import { handleValidationError } from '../errors/handleValidationError'
 import ApiError from '../errors/ApiError'
+import { errorLogger } from '../shared/logger'
 
 const globalErrorHandler: ErrorRequestHandler = (
   error,
@@ -13,6 +16,8 @@ const globalErrorHandler: ErrorRequestHandler = (
   let statusCode = 500
   let message = 'Something Went Wrong'
   let errorMessages: IGenericErrorMessages[] = []
+
+  config.env === 'development' ? console.log(error) : errorLogger.error(error)
 
   if (error?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(error)
